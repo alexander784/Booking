17d3769d12a9cad5c1bab1a814dcce097ff0ef73
alexander <alexandernyaga1999@gmail.com>
2023-12-18 09:46:39 +0300

    Create list page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { list } from "./pages/list/list";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/hotels" element={<list/>}/>
     </Routes>
     </BrowserRouter>
   );

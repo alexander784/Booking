5476e08ecaff1c9b17d96267ee6324bc581ccf76
alexander <alexandernyaga1999@gmail.com>
2023-12-18 09:27:45 +0300

    Routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <BrowserRouter>
+    <Routes path="/" element={}>
+    </Routes>
+    <Route></Route>
+    </BrowserRouter>
   );
 }
 

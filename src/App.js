0881a4ce407368a4bcefa4a760e551a53385d225
alexander <alexandernyaga1999@gmail.com>
2023-12-18 09:38:42 +0300

    Home function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes path="/" element={}>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
     </Routes>
-    <Route></Route>
     </BrowserRouter>
   );
 }

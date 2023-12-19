12a755932b699aaa77d45a4846ab37063bdf1b9c
alexander <alexandernyaga1999@gmail.com>
2023-12-19 07:31:04 +0300

    navbar buttons

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,17 @@
 import "./home.css"
 export const Home = () => {
   return (
-    
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">Lexabooking</span>
+        <div className="navitems">
+          <button className="navButton">Register</button>
+          <button className="navButton">login</button>
+        </div>
+        
+        </div>
+
+      </div>
   )
 }
 

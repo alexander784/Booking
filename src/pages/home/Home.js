c212a1437e794a42230b107e29735e73a614b864
alexander <alexandernyaga1999@gmail.com>
2023-12-19 07:57:11 +0300

    Create a header component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,15 @@
 import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/navbar/header/Header";
 import "./home.css"
 
 const Home = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <Navbar />
+      <Header />
+
+    </div>
+    
   )
 }
 

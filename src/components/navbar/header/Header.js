63ff2d26eeba8cdee65cd11ca0294e10146f9bdf
alexander <alexandernyaga1999@gmail.com>
2023-12-19 08:44:24 +0300

    Add header description

--- src/components/navbar/header/Header.js
+++ src/components/navbar/header/Header.js
@@ -1,23 +1,23 @@
  
  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css"
-import { faBed } from "@fortawesome/free-solid-svg-icons";
+import { faBed, faCar, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons";
  const Header = () => {
    return (
      <div className='header'>
         <div className="headerList">
-            <div className="headerListItem">
+            <div className="headerListItem active"> 
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
             </div>
 
             <div className="headerListItem">
-            <FontAwesomeIcon icon={faBed} />
+            <FontAwesomeIcon icon={faPlane} />
             <span>Flights</span>
             </div>
 
             <div className="headerListItem">
-            <FontAwesomeIcon icon={faBed} />
+            <FontAwesomeIcon icon={faCar} />
             <span>Car rentals</span>
             </div>
 
@@ -27,11 +27,14 @@ import { faBed } from "@fortawesome/free-solid-svg-icons";
             </div>
 
             <div className="headerListItem">
-            <FontAwesomeIcon icon={faBed} />
+            <FontAwesomeIcon icon={faTaxi} />
             <span>Airport Taxis</span>
-
             </div>
         </div>
+        <h1 className="headerTitle"> A LifeTime discount? It's Genius.</h1>
+        <p className="headerDesc">
+            Get rewarded for your travels unlock instant savings of 10% or more with free lexabookings account.
+        </p>
 
         </div>
    )

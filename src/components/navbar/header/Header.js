6789d46f1839db472bd21b079a3f58e9eac9444d
alexander <alexandernyaga1999@gmail.com>
2023-12-19 08:10:24 +0300

    Install icons packages

--- src/components/navbar/header/Header.js
+++ src/components/navbar/header/Header.js
@@ -1,9 +1,19 @@
  
- import React from 'react'
- 
+ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./header.css"
+import { faBed } from "@fortawesome/free-solid-svg-icons";
  const Header = () => {
    return (
-     <div className='header'>Header</div>
+     <div className='header'>
+        <div className="headerList">
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+            
+            </div>
+        </div>
+
+        </div>
    )
  }
 

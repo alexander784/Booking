e41670d7e7caf9097865c58cba9adb4642c8198e
alexander <alexandernyaga1999@gmail.com>
2023-12-20 17:47:23 +0300

    Customize Featured Properties

--- src/components/FeaturedProperties/FeaturedProperties.js
+++ src/components/FeaturedProperties/FeaturedProperties.js
@@ -3,6 +3,61 @@ import "./FeaturedProperties.css"
 
 const FeaturedProperties = () => {
   return (
- 
+    <div className='fp'>
+        <div className='fpItem'>
+        <img src='https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fE5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D' 
+            alt=''
+            className='fpImg' />
+        <span className='fpName'>Aparthotel Stare Miasto</span>
+        <span className='fpCity'>Madrid</span>
+        <span className='fpPrice'>Starting from $120</span>
+        <div className='fpRating'>
+            <button>8.9</button>
+            <span>Excellent</span>
+            </div>
+        </div>
+
+        <div className='fpItem'>
+        <img src='https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fE5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D' 
+            alt=''
+            className='fpImg' />
+        <span className='fpName'>Aparthotel Stare Miasto</span>
+        <span className='fpCity'>Madrid</span>
+        <span className='fpPrice'>Starting from $120</span>
+        <div className='fpRating'>
+            <button>8.9</button>
+            <span>Excellent</span>
+            </div>
+            </div>
+
+            <div className='fpItem'>
+            <img src='https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fE5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D' 
+            alt=''
+            className='fpImg' />
+        <span className='fpName'>Aparthotel Stare Miasto</span>
+        <span className='fpCity'>Madrid</span>
+        <span className='fpPrice'>Starting from $120</span>
+        <div className='fpRating'>
+            <button>8.9</button>
+            <span>Excellent</span>
+            </div>
+            </div>
+
+            <div className='fpItem'>
+            <img src='https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fE5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D' 
+            alt=''
+            className='fpImg' />
+        <span className='fpName'>Aparthotel Stare Miasto</span>
+        <span className='fpCity'>Madrid</span>
+        <span className='fpPrice'>Starting from $120</span>
+        <div className='fpRating'>
+            <button>8.9</button>
+            <span>Excellent</span>
+            </div>
+        </div>
+        </div>
+
+  )
+}
 
 export default FeaturedProperties;

459faa3634d56e08c49f4b155c6b4f852a8a22e1
alexander <alexandernyaga1999@gmail.com>
2023-12-22 05:13:35 +0300

    Add list items

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/navbar/header/Header';
+import "./list.css"
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list"  />
+      <div className='listContainer'>
+        <div className='listWrapper'>
+          <div className='listSearch'>
+            <h1 className='lsTitle'>Search</h1>
+            <div className='listResult'></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

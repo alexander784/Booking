56a3750fdebc9fe8ab5fc3b2998e7b5635902249
alexander <alexandernyaga1999@gmail.com>
2023-12-28 10:23:59 +0300

    Add search details

--- src/components/searchItem/SearchItem.js
+++ src/components/searchItem/SearchItem.js
@@ -25,7 +25,17 @@ const SearchItem = () => {
 
         </span>
       </div>
-        <div className="siDetails">details</div>
+        <div className="siDetails">
+          <div className="siRating">
+            <span>Excellent</span>
+            <button>8.9</button>
+          </div>
+          <div className="siDetailTexts">
+            <span className="siPrice">$123</span>
+            <span className="siTaxOp">Includes taxes and fees</span>
+            <button className="siCheckButton">See availability</button>
+          </div>
+        </div>
         
       
     </div>

668db32df00ee0be0ffbb24102b6db26bcc3f992
alexander <alexandernyaga1999@gmail.com>
2023-12-28 16:23:25 +0300

    Add images

--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -3,7 +3,30 @@ import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLocation, faLocationDot } from "@fortawesome/free-solid-svg-icons"
+import { parseWithOptions } from "date-fns/fp"
 const Hotel = () => {
+
+  const photos = [
+    {
+      src: "https://unsplash.com/photos/an-aerial-view-of-a-city-with-a-bridge-SVGJsMVjEF4"
+    },
+    {
+      src: "https://unsplash.com/photos/an-aerial-view-of-a-city-with-a-bridge-SVGJsMVjEF4"
+    },
+    {
+      src: "https://unsplash.com/photos/an-aerial-view-of-a-city-with-a-bridge-SVGJsMVjEF4"
+    },
+    {
+      src: "https://unsplash.com/photos/an-aerial-view-of-a-city-with-a-bridge-SVGJsMVjEF4"
+    },
+    {
+      src: "https://unsplash.com/photos/an-aerial-view-of-a-city-with-a-bridge-SVGJsMVjEF4"
+    },
+    {
+      src: "https://unsplash.com/photos/an-aerial-view-of-a-city-with-a-bridge-SVGJsMVjEF4"
+    }
+    
+  ]
   return (
     <div>
       <Navbar />
@@ -21,9 +44,38 @@ const Hotel = () => {
           <span className="hotelPriceHighlight">
             Book a stay over $114 at this property and get a free airport taxi
           </span>
+          <div className="hotelImages">
+            {photos.map(photo=>(
+              <div className="hotelImgWrapper">
+                <img src="photo.src" alt="" className="hotelImg" />
         </div>
 
+            ))}
+
+          </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Stay in the heart of Kravow</h1>
+              <p className="hotelDesc">
+                Located a 5 minute walk fro st.florian
+              </p>
+            </div>
+            <div className="hotelDetailslsPrice">
+              <h1>Perfect for a 0-night stay</h1>
+              <span>
+                Located in the real heart of kravow, this property has an Excellentlocation of 9.8
+
+              </span>
+
+              <h2>
+                <b>$945</b> (9 nights)
+              </h2>
+              <button>Reserve or Book Now</button>
+            </div>
+          </div>
+
       </div>
+    </div>
     </div>
   )
 }
